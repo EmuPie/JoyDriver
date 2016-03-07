c9d9dec5d1427467c20c6caaf9fe712f8d28cbbf
Rickard Doverfelt <rickard.doverfelt@gmail.com>
2016-03-07 18:29:25 +0100

    Cleaned up the code

--- Driver.py
+++ Driver.py
@@ -3,11 +3,14 @@
 # Written by Limor "Ladyada" Fried for Adafruit Industries, (c) 2015
 # This code is released into the public domain
 
+# Adapted for <insert ADC name here> by Rickard Doverfelt
+
 import time
 import os
 import RPi.GPIO as GPIO
 import uinput
 
+# Define used keys from uinput
 device = uinput.Device([
 	uinput.KEY_W,
 	uinput.KEY_S,
@@ -20,9 +23,8 @@ device = uinput.Device([
 	])
 
 GPIO.setmode(GPIO.BCM)
-DEBUG = 1
 
-# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+# read SPI data from <insert ADC name here> chip, 4 possible adc's (0 thru 3)
 def readadc(adcnum, clockpin, mosipin, misopin, cspin):
         if ((adcnum > 3) or (adcnum < 0)):
                 return -1
@@ -41,9 +43,8 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
 		commandout = 13
 	elif (adcnum == 3):
 		commandout = 15
-	
-        #commandout |= 0x18  # start bit + single-ended bit
-        commandout <<= 4    # we only need to send 5 bits here
+
+        commandout <<= 4    # we only need to send 4 bits here
         for i in range(4):
                 if (commandout & 0x80):
                         GPIO.output(mosipin, True)
@@ -68,7 +69,7 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
         return adcout
 
 # change these as desired - they're the pins connected from the
-# SPI port on the ADC to the Cobbler
+# SPI port on the ADC to the Pi
 SPICLK = 18
 SPIMISO = 23
 SPIMOSI = 24
@@ -80,22 +81,15 @@ GPIO.setup(SPIMISO, GPIO.IN)
 GPIO.setup(SPICLK, GPIO.OUT)
 GPIO.setup(SPICS, GPIO.OUT)
 
-# 10k trim pot connected to adc #0
+# The channel number for each axis
 joyX1 = 0
 joyY1 = 1
 joyX2 = 2
 joyY2 = 3
 
-last_read = 0       # this keeps track of the last potentiometer value
-tolerance = 5       # to keep from being jittery we'll only change
-                    # volume when the pot has moved more than 5 'counts'
-
 while True:
 	
-	#print("X1: " + str(readadc(joyX1, SPICLK, SPIMOSI, SPIMISO, SPICS)))
-	#print("Y1: " + str(readadc(joyY1, SPICLK, SPIMOSI, SPIMISO, SPICS)))
-	#print("X2: " + str(readadc(joyX2, SPICLK, SPIMOSI, SPIMISO, SPICS)))
-	#print("Y2: " + str(readadc(joyY2, SPICLK, SPIMOSI, SPIMISO, SPICS)))
+	# Do the input logic
 	if (readadc(joyX1, SPICLK, SPIMOSI, SPIMISO, SPICS) < 924):
 		device.emit_click(uinput.KEY_A)
 	elif (readadc(joyX1, SPICLK, SPIMOSI, SPIMISO, SPICS) > 1124):
